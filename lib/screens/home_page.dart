09dfd0c643b54cc39435e1b09c6750aceb3ea6c0
miguelherrerad <57775873+miguelherrerad@users.noreply.github.com>
2022-03-18 16:46:54 -0500

    Assets Images

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:rent_house_app/theme.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: whiteColor,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //NOTE: Reader
+              Padding(
+                padding: EdgeInsets.only(left: 30.0, top: 30.0),
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Image.asset(
+                    "assets/images/menu_icon.png",
+                    width: 18,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }

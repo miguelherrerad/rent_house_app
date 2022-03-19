a66c242e3d8632ea243466d4ddb79b538cd69c08
miguelherrerad <57775873+miguelherrerad@users.noreply.github.com>
2022-03-18 22:19:08 -0500

    PageDetails Screen - Title and Rating

--- lib/screens/detail_page.dart
+++ lib/screens/detail_page.dart
@@ -22,14 +22,74 @@ class DetailPage extends StatelessWidget {
                 fit: BoxFit.cover,
               ),
             ),
+            ListView(
+              children: [
+                SizedBox(
+                  height: 266,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    borderRadius:
+                        BorderRadius.vertical(top: Radius.circular(30)),
+                    color: whiteColor,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      //NOTE: title
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 30,
+                          vertical: 24,
+                        ),
+                        child: Row(
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Casa Moderna",
+                                  style: secondaryTitle,
+                                ),
+                                SizedBox(
+                                  height: 4,
+                                ),
+                                Text(
+                                  "Zamora Hoayco, Loja",
+                                  style: infoSecondaryTitle,
+                                ),
+                              ],
+                            ),
+                            Spacer(),
+                            Row(
+                              children: [1, 2, 3, 4, 5].map((e) {
+                                return Icon(
+                                  Icons.star,
+                                  size: 12,
+                                  color: (e <= 5) ? orangeColor : greyColor,
+                                );
+                              }).toList(),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
             //NOTE: button back
             Positioned(
               top: 20,
               left: 20,
               child: MaterialButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.pop(context);
+                },
                 minWidth: 30,
                 height: 30,
+                elevation: 6,
                 padding: EdgeInsets.all(5),
                 color: whiteColor,
                 shape: RoundedRectangleBorder(
@@ -40,7 +100,7 @@ class DetailPage extends StatelessWidget {
                   size: 14,
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
